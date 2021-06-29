d3ed087dd04e2f3160e8291e86fcb7507ff3e221
mdabrowski25 <mdabrowski2511@gmail.com>
2021-06-29 10:36:07 +0200

    shuffle method tweak

--- src/app/generator/generator.component.ts
+++ src/app/generator/generator.component.ts
@@ -3,7 +3,6 @@ import { RacerTeamDto } from '../shared/dto/racer-team-dto.model';
 import { Racer } from '../shared/model/racer.model';
 import { Team } from '../shared/model/team.model';
 import { HttpService } from '../services/http.service';
-import { map } from 'rxjs/operators';
 
 @Component({
     selector: 'app-generator',
@@ -11,29 +10,19 @@ import { map } from 'rxjs/operators';
     styleUrls: ['./generator.component.css']
 })
 export class GeneratorComponent implements OnInit {
-    racers = [
-        new Racer(1,'Piotrula'),
-        new Racer(2,'Daber'),
-        new Racer(3,'Juri'),
-        new Racer(4,'Werty'),
-        new Racer(5,'Kopyt'),
-        new Racer(6,'Zbychu'),
-        new Racer(7,'Ginol'),
-        new Racer(8,'grzywek'),
-        new Racer(9,'BlyatMAN')
-    ];
+    racers: Racer[] = [];
 
     teams = [
-        new Team(1,'Red Bull Racing'),
-        new Team(2,'Red Bull Racing'),
-        new Team(3,'Renault'),
-        new Team(4,'Renault'),
-        new Team(5,'Ferrari'),
-        new Team(6,'Ferrari'),
-        new Team(7,'Mercedes'),
-        new Team(8,'Mercedes'),
-        new Team(9,'McLaren'),
-        new Team(10,'McLaren')
+        new Team(1, 'Red Bull Racing'),
+        new Team(2, 'Red Bull Racing'),
+        new Team(3, 'Renault'),
+        new Team(4, 'Renault'),
+        new Team(5, 'Ferrari'),
+        new Team(6, 'Ferrari'),
+        new Team(7, 'Mercedes'),
+        new Team(8, 'Mercedes'),
+        new Team(9, 'McLaren'),
+        new Team(10, 'McLaren')
     ];
 
     racerTeam: RacerTeamDto[] = [];
@@ -42,15 +31,17 @@ export class GeneratorComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        // this.http.getRacers().subscribe((data) => {
-        //     this.racers = data.racers;
-        // });
+        this.http.getRacers().subscribe((data) => {
+            this.racers = data.racers;
+        });
     }
 
-    shuffleRacerAndTeam(): RacerTeamDto {
+    shuffleRacerAndTeam(): void {
         const randomRacerInt = Math.floor(Math.random() * this.racers.length);
 
-        if (this.racers.length === 0){return new RacerTeamDto(new Racer(69,"*"), new Team(69,"*"))}
+        if (this.racers.length === 0) {
+            return;
+        }
 
         let shuffledRacer = this.racers[randomRacerInt];
         this.racers = this.racers.filter((e) => {
@@ -59,7 +50,9 @@ export class GeneratorComponent implements OnInit {
 
         const randomTeamInt = Math.floor(Math.random() * this.teams.length);
 
-        if (this.teams.length === 0){return new RacerTeamDto(new Racer(69,"*"), new Team(69,"*"))}
+        if (this.teams.length === 0) {
+            return;
+        }
 
         let shuffledTeam = this.teams[randomTeamInt];
         this.teams = this.teams.filter((e) => {
@@ -68,6 +61,5 @@ export class GeneratorComponent implements OnInit {
 
         let racerTeamDto = new RacerTeamDto(shuffledRacer, shuffledTeam);
         this.racerTeam.push(racerTeamDto);
-        return racerTeamDto;
     }
 }
