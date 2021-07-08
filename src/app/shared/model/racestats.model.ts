c8e0c673e0ccc7e362b76b0c6fc9ddf007dca9ba
mdabrowski25 <mdabrowski2511@gmail.com>
2021-07-08 12:54:57 +0200

    racestats implemented in general-classification

--- src/app/shared/model/racestats.model.ts
+++ src/app/shared/model/racestats.model.ts
@@ -1,16 +1,23 @@
 import { Team } from './team.model';
+import { Racer } from './racer.model';
 
 export class RaceStats {
+    raceId: number;
+    driver: Racer;
     raceName: string;
     raceDate: string;
+    position: number;
     personalBestLapTime: number[];
     teamDriven: Team;
 
 
-    constructor(raceName: string, raceDate: string, personalBestLapTime: number[], teamDriven: Team) {
+    constructor(raceId: number, raceName: string, raceDate: string, personalBestLapTime: number[], teamDriven: Team , driverId: Racer, position: number) {
+        this.raceId = raceId;
         this.raceName = raceName;
         this.raceDate = raceDate;
+        this.position = position;
         this.personalBestLapTime = personalBestLapTime;
         this.teamDriven = teamDriven;
+        this.driver = driverId;
     }
 }
--- src/app/standings/general-classification/general-classification.component.ts
+++ src/app/standings/general-classification/general-classification.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Race } from '../../shared/model/race.model';
 import { Racer } from '../../shared/model/racer.model';
+import { RaceStats } from '../../shared/model/racestats.model';
 
 @Component({
     selector: 'app-general-classification',
@@ -10,75 +11,52 @@ import { Racer } from '../../shared/model/racer.model';
 export class GeneralClassificationComponent implements OnInit {
     @Input() races: Race[] = [];
     @Input() racers: Racer[] = [];
+    allRacesStats: RaceStats[] = [];
 
     constructor() {
     }
 
     ngOnInit(): void {
+        this.getAllRacesStats()
     }
 
-
-    getRacerPosition(id: number, raceId: number) {
-        let race = this.races.find(race => race.id == raceId);
-        if (race != undefined) {
-            if (race.scores != undefined) {
-                for (let i = 0; i < race.scores.length; i++) {
-                    if (race.scores[i].racerAndTeam.racer.id == id) {
-                        return race.scores[i].position;
-                    }
+    getAllRacesStats() {
+        let raceStats: RaceStats[] = [];
+        for (let i = 0; i < this.races.length; i++) {
+            let scores = this.races[i].scores;
+            let name = this.races[i].name;
+            let date = this.races[i].date;
+            let raceId = this.races[i].id
+            if (scores != undefined && date != undefined) {
+                for (let j = 0; j < scores.length; j++) {
+                    let racerStatObj = new RaceStats(
+                       raceId, name, date, scores[j].bestLapTime, scores[j].racerAndTeam.team, scores[j].racerAndTeam.racer, scores[j].position
+                    );
+                    raceStats.push(racerStatObj);
                 }
             }
         }
-        return null;
+        this.allRacesStats = raceStats;
     }
 
-    getRacerTeam(id: number, raceId: number) {
-        let race = this.races.find(race => race.id == raceId);
-        if (race != undefined) {
-            if (race.scores != undefined) {
-                for (let i = 0; i < race.scores.length; i++) {
-                    if (race.scores[i].racerAndTeam.racer.id == id) {
-                        return race.scores[i].racerAndTeam.team.name;
-                    }
-                }
+    getRacersStatsFromRace(raceId: number) {
+        let raceStats1 = this.allRacesStats.filter(race => race.raceId == raceId).slice();
+        raceStats1.sort((a, b) => {
+            if (a.driver.id > b.driver.id) {
+                return 1;
+            } else if (a.driver.id == b.driver.id) {
+                return 0;
+            } else {
+                return -1;
             }
-        }
-        return null;
+        });
+        return raceStats1;
     }
 
-    getRacerLapTime(id: number, raceId: number) {
-        let race = this.races.find(race => race.id == raceId);
-        if (race != undefined) {
-            if (race.scores != undefined) {
-                for (let i = 0; i < race.scores.length; i++) {
-                    if (race.scores[i].racerAndTeam.racer.id == id) {
-                        return race.scores[i].bestLapTime;
-                    }
-                }
-            }
-        }
-        return null;
+    getRacerStatsFromRace(racerId: number, raceId: number) {
+        let racersStatsFromRace = this.getRacersStatsFromRace(raceId);
+        return racersStatsFromRace[racerId - 1];
     }
 
-    getRacerEarnedPoints(id: number, raceId: number) {
-        let race = this.races.find(race => race.id == raceId);
-        if (race != undefined) {
-            if (race.scores != undefined) {
-                for (let i = 0; i < race.scores.length; i++) {
-                    if (race.scores[i].racerAndTeam.racer.id == id) {
-                        return race.scores[i].racerAndTeam.racer.points;
-                    }
-                }
-            }
-        }
-        return null;
-    }
 
-    getRacerLapTimeString(id: number, raceId: number): string {
-        let racerLapTime = this.getRacerLapTime(id, raceId);
-        if (racerLapTime != null) {
-            return racerLapTime[0] + ':' + racerLapTime[1] + ':' + racerLapTime[2];
-        }
-        return '';
-    }
 }
