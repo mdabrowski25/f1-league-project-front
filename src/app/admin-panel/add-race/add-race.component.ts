f9707231955adf38bb3659457e08a63a157acbb7
mdabrowski25 <mdabrowski2511@gmail.com>
2021-07-10 08:56:34 +0200

    admin-panel changes

--- src/app/admin-panel/add-race/add-race.component.ts
+++ src/app/admin-panel/add-race/add-race.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { DatePipe } from '@angular/common';
 
 @Component({
-  selector: 'app-add-race',
-  templateUrl: './add-race.component.html',
-  styleUrls: ['./add-race.component.css']
+    selector: 'app-add-race',
+    templateUrl: './add-race.component.html',
+    styleUrls: ['./add-race.component.css']
 })
 export class AddRaceComponent implements OnInit {
+    raceForm: FormGroup;
 
-  constructor() { }
+    constructor(private datePipe: DatePipe) {
+        this.raceForm = new FormGroup({
+            raceName: new FormControl(null, Validators.required),
+            raceDate: new FormControl(null, Validators.required)
+        });
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
+    onSubmit() {
+        const raceFromForm = {
+            name: this.raceForm.value.raceName,
+            date: this.datePipe.transform(this.raceForm.value.raceDate, 'dd-MM-yyyy HH:mm')
+        }
+        console.log(raceFromForm)
+    }
 }
